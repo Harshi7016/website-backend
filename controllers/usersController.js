437bbdc78ab4589c8f2ac292fea58c38fb565012
Harshith7016 <harshithece7016@gmail.com>
2021-02-25 04:29:53 +0530

    test auth

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 
 const userQuery = require('../models/users')
+const accountOwners = require('../mocktestdata/appOwners.json')
 
 /**
  * Fetches the data about our users
@@ -19,6 +20,22 @@ const getUsers = async (req, res) => {
   } catch (error) {
     logger.error(`Error while fetching all users: ${error}`)
     return res.boom.serverUnavailable('Something went wrong please contact admin')
+  }
+}
+
+/**
+ * Fetches the data about our account Owners
+ *
+ * @param req {Object} - Express request object
+ * @param res {Object} - Express response object
+ */
+
+const getAccountOwners = async (req, res) => {
+  try {
+    return accountOwners
+  } catch (error) {
+    logger.error(`Error while fetching application owners: ${error}`)
+    return res.boom.badImplementation('Something went wrong please contact admin')
   }
 }
 
@@ -106,5 +123,6 @@ module.exports = {
   updateSelf,
   getUsers,
   getSelfDetails,
-  getUser
+  getUser,
+  getAccountOwners
 }
